dacdc7f315c2b43ba81ac04978a69ed4164ec8e6
Joshua Farayola <farayolajoshua@gmail.com>
2023-02-16 11:12:44 +0100

    Set up validation pipe and serializer interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      strategy: 'excludeAll',
+    }),
+  );
+
   await app.listen(3000);
 }
 
